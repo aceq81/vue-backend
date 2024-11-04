01ec93970bb928cb32fc90d30789920ee922ea85
aceq81 <94726338+aceq81@users.noreply.github.com>
2024-11-04 22:49:03 +0900

    Initial commit

---
+++ src/main/java/com/example/vuebackboard/entity/BoardRepository.java
@@ -0,0 +1,5 @@
+package com.example.vuebackboard.entity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
+}
